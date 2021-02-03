e9e91ae736c3a0b076bae8d9f4ece947643ccd5c
sagarp96 <sagarcool2345@gmail.com>
2021-02-03 20:08:05 +0530

    commits

---
+++ phoneAndEmail.py
@@ -0,0 +1,35 @@
+# phoneAndEmail.py - just copy the emails and phone numbers from webpages and phone and emails will be seperated
+import  re
+import  pyperclip
+
+phoneRegex = re.compile(r'''(
+(\d{3}|\(\d{3}\))? # area code
+(\s|-|\.)? # separator
+(\d{3}) # first 3 digits
+(\s|-|\.) # separator
+(\d{4}) # last 4 digits
+(\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+)''', re.VERBOSE)
+emailRegex=re.compile(r'''(
+[a-zA-Z0-9._%+-]+ 
+@               # at the rate
+[a-zA-Z0-9.-]+ # domain name
+(\.[a-zA-Z]{2,4}) # dot-something
+)''' ,re.VERBOSE)
+
+text = str(pyperclip.paste())
+
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+if len(matches)>0:
+    pyperclip.copy('\n'.join(matches)) #copying the returned string from the program
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a simple one page python program which reads your text that you have copied and scraps out the email and phone number from it 
+the phone number format is in US country code and email formal is asual
+first copy the whole text and just run the program,voila! all your emails and phone numbers have been scrapped
